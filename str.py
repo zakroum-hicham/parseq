8c4547d056c17ed0f269e48a35401598118d2195
Hicham Zakroum <hicham.zakroum2@gmail.com>
2025-04-17 14:52:11 +0200

    commit

--- str.py
+++ str.py
@@ -29,7 +29,7 @@ import torch
 
 from tqdm import tqdm
 
-from .model.help import load_from_checkpoint, parse_model_args
+from model.help import load_from_checkpoint, parse_model_args
 
 from PIL import Image
 import json
