1c4068950cfed855b092bd56002024ac0dbd3017
Hicham Zakroum <hicham.zakroum2@gmail.com>
2025-04-17 14:54:19 +0200

    commit

--- model/help.py
+++ model/help.py
@@ -1,9 +1,5 @@
 from .system import PARSeq
 
-from typing import Sequence
-
-from torch import nn
-
 
 class InvalidModelError(RuntimeError):
     """Exception raised for any model-related error (creation, loading)"""
@@ -25,23 +21,3 @@ def parse_model_args(args):
         name, arg_type = name.split(':', maxsplit=1)
         kwargs[name] = arg_types[arg_type](value)
     return kwargs
-
-def init_weights(module: nn.Module, name: str = '', exclude: Sequence[str] = ()):
-    """Initialize the weights using the typical initialization schemes used in SOTA models."""
-    if any(map(name.startswith, exclude)):
-        return
-    if isinstance(module, nn.Linear):
-        nn.init.trunc_normal_(module.weight, std=.02)
-        if module.bias is not None:
-            nn.init.zeros_(module.bias)
-    elif isinstance(module, nn.Embedding):
-        nn.init.trunc_normal_(module.weight, std=.02)
-        if module.padding_idx is not None:
-            module.weight.data[module.padding_idx].zero_()
-    elif isinstance(module, nn.Conv2d):
-        nn.init.kaiming_normal_(module.weight, mode='fan_out', nonlinearity='relu')
-        if module.bias is not None:
-            nn.init.zeros_(module.bias)
-    elif isinstance(module, (nn.LayerNorm, nn.BatchNorm2d, nn.GroupNorm)):
-        nn.init.ones_(module.weight)
-        nn.init.zeros_(module.bias)
--- model/system.py
+++ model/system.py
@@ -28,8 +28,29 @@ from pytorch_lightning.utilities.types import STEP_OUTPUT
 from timm.models.helpers import named_apply
 
 from .base import CrossEntropySystem
-from .help import init_weights
 from .modules import DecoderLayer, Decoder, Encoder, TokenEmbedding
+
+
+def init_weights(module: nn.Module, name: str = '', exclude: Sequence[str] = ()):
+    """Initialize the weights using the typical initialization schemes used in SOTA models."""
+    if any(map(name.startswith, exclude)):
+        return
+    if isinstance(module, nn.Linear):
+        nn.init.trunc_normal_(module.weight, std=.02)
+        if module.bias is not None:
+            nn.init.zeros_(module.bias)
+    elif isinstance(module, nn.Embedding):
+        nn.init.trunc_normal_(module.weight, std=.02)
+        if module.padding_idx is not None:
+            module.weight.data[module.padding_idx].zero_()
+    elif isinstance(module, nn.Conv2d):
+        nn.init.kaiming_normal_(module.weight, mode='fan_out', nonlinearity='relu')
+        if module.bias is not None:
+            nn.init.zeros_(module.bias)
+    elif isinstance(module, (nn.LayerNorm, nn.BatchNorm2d, nn.GroupNorm)):
+        nn.init.ones_(module.weight)
+        nn.init.zeros_(module.bias)
+
 
 
 class PARSeq(CrossEntropySystem):
